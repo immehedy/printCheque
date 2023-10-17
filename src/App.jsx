a88b85875e0440ce97bc11d8a16c48fc2b75ce87
mehedy hassan <mehedy@algosolver.com>
2023-10-17 18:05:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import "./App.css";
+import { useRef, useState } from "react";
+import { useReactToPrint } from "react-to-print";
+import PrintSection from "./PrintSection";
+function App() {
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  const [payee, setPayee] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState("");
+  const [isAcPayee, setIsAcPayee] = useState(false);
+
+  return (
+    <>
+      <div className="p-4 max-w-5xl border rounded-md mx-auto mt-24">
+        <h1 className="text-2xl font-bold text-center">Cheque Printing</h1>
+
+        <div className="mt-4">
+          <label className="block mb-2">Payee Name:</label>
+          <input
+            type="text"
+            value={payee}
+            onChange={(e) => setPayee(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded"
+          />
+        </div>
+
+        <div className="mt-4">
+          <label className="block mb-2">Amount (BDT):</label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded"
+          />
+        </div>
+
+        <div className="mt-4">
+          <label className="block mb-2">Date:</label>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded"
+          />
+        </div>
+
+        <div className="mt-4">
+          <label className="block mb-2">
+            <input
+              type="checkbox"
+              checked={isAcPayee}
+              onChange={() => setIsAcPayee(!isAcPayee)}
+              className="mr-2"
+            />
+            AC Payee Only
+          </label>
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            onClick={handlePrint}
+            className="mt-6 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+            Print Cheque
+          </button>
+        </div>
+      </div>
+      <div className="opacity-0">
+        <div ref={componentRef}>
+          <PrintSection
+            amount={amount}
+            date={date}
+            payee={payee}
+            isAcPayee={isAcPayee}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/PrintSection.jsx
@@ -0,0 +1,60 @@
+import { ToWords } from "to-words";
+
+const PrintSection = ({ payee, amount, date, isAcPayee }) => {
+  const toWords = new ToWords({
+    localeCode: "en-BD",
+    converterOptions: {
+      currency: true,
+      ignoreDecimal: false,
+      ignoreZeroCurrency: false,
+      doNotAddOnly: false,
+      currencyOptions: {
+        name: "Taka",
+        plural: "Taka",
+        symbol: "৳",
+        fractionalUnit: {
+          name: "Paisa",
+          plural: "Paisa",
+          symbol: "",
+        },
+      },
+    },
+  });
+  return (
+    <div className="relative">
+      {isAcPayee && (
+        <span className="absolute mt-12 border-t border-b -rotate-45">
+          A/C PAYEE ONLY
+        </span>
+      )}
+
+      <p className="absolute mt-[120.94488189px] ml-[85.590551181px] ">
+        {payee ?? "Mehedy Hassan"}
+      </p>
+      <p className="absolute mt-[158.74015748px] ml-[910.23622047px]">
+        {amount ?? "0"} /-
+      </p>
+      <p className="absolute w-[359.05511811px] mt-[170.07874016px] ml-[132.28346457px]">
+        {amount ? toWords.convert(amount) : toWords.convert(0)}
+      </p>
+      {/* <p className="absolute mt-[170.07874016px] ml-[132.28346457px]">
+        {amount ?? "0"} /-
+      </p> */}
+
+      <div className="absolute mt-[64.251968504px] w-full  px-[26.456692913px] flex justify-end items-center">
+        <p className="w-[22.677165354px]">
+          {date ? date?.split("-")[2][0] : 0}
+        </p>
+        <p className="w-[22.677165354px]">{date ? date.split("-")[2][1] : 0}</p>
+        <p className="w-[22.677165354px]">{date ? date.split("-")[1][0] : 0}</p>
+        <p className="w-[22.677165354px]">{date ? date.split("-")[1][1] : 0}</p>
+        <p className="w-[22.677165354px]">{date ? date.split("-")[0][0] : 0}</p>
+        <p className="w-[22.677165354px]">{date ? date.split("-")[0][1] : 0}</p>
+        <p className="w-[22.677165354px]">{date ? date.split("-")[0][2] : 0}</p>
+        <p className="w-[22.677165354px]">{date ? date.split("-")[0][3] : 0}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PrintSection;
