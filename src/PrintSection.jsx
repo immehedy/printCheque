71ea4a02709dd83fef60aa0cbd79705ccc7f7fa6
mehedy hassan <mehedy@algosolver.com>
2023-10-19 14:37:09 +0600

    redesign

--- src/PrintSection.jsx
+++ src/PrintSection.jsx
@@ -23,24 +23,26 @@ const PrintSection = ({ payee, amount, date, isAcPayee }) => {
   return (
     <div className="relative">
       {isAcPayee && (
-        <span className="absolute mt-12 ml-6 border-t border-b -rotate-45">
+        <span className="absolute mt-14 border-t border-b -rotate-45 font-bold">
           A/C PAYEE ONLY
         </span>
       )}
 
       {/* <p className="absolute mt-[120.94488189px] ml-[85.590551181px] "> */}
-      <p className="absolute mt-[120.94488189px] ml-[87.590551181px] ">
+        <p className="absolute mt-[120.94488189px] ml-[87.590551181px] ">
         {payee ?? "Mehedy Hassan"}
       </p>
       {/* <p className="absolute mt-[158.74015748px] ml-[910.23622047px]"> */}
-      <p className="absolute mt-[162.74015748px] right-8">{amount ?? "0"} /-</p>
+        <p className="absolute mt-[162.74015748px] right-8">
+        {amount ?? "0"} /-
+      </p>
       {/* <p className="absolute w-[359.05511811px] mt-[170.07874016px] ml-[132.28346457px]"> */}
-      <p className="absolute w-[359.05511811px] mt-[170.07874016px] ml-[134.28346457px]">
+        <p className="absolute w-[359.05511811px] mt-[170.07874016px] ml-[134.28346457px]">
         {amount ? toWords.convert(amount) : toWords.convert(0)}
       </p>
 
       {/* <div className="absolute mt-[64.251968504px] w-full  px-[26.456692913px] flex justify-end items-center"> */}
-      <div className="absolute mt-[64.251968504px] w-full  px-[46.456692913px] flex justify-end items-center">
+        <div className="absolute mt-[64.251968504px] w-full  px-[46.456692913px] flex justify-end items-center">
         <p className="w-[22.677165354px]">
           {date ? date?.split("-")[2][0] : 0}
         </p>
